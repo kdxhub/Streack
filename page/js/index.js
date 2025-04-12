cf9d67597e9d303984713b6a3d70fc89ff424f79
kdxiaoyi [mipad7] <kdxiaoyi@outlook.com>
2025-04-12 11:58:25 +0000

    new  home page

--- page/js/index.js
+++ page/js/index.js
@@ -1,3 +1,67 @@
+//定义页面元素集合
+pageElements = {
+  root: document.getElementById("page"),
+  no_script: document.getElementById("no_script"),
+  main: {
+    root: document.getElementById("main"),
+    slot: [],
+  },
+};
+pageElements.main.slot = document.querySelectorAll("div[slot='true']");
 
+//API
+function getQueryString(name) { let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); let r = window.location.search.substr(1).match(reg); if (r != null) { return unescape(r[2]); }; return null; };
+function openURL(URI, IsInPresentWindow) {
+  let linkEle = document.createElement("a");
+  if (!!IsInPresentWindow) {
+    linkEle.target = "_self";
+  } else {
+    linkEle.target = "_blank";
+  };
+  linkEle.href = URI;
+  linkEle.click();
+  return linkEle;
+};
+function msg(Message, ConfirmBtnText, isWarning, duration, onclick, align, icon) {
+  let infoJSON = {
+    root: pmdElements.pageRoot,
+    text: Message,
+    type: "basic",
+    action: {},
+  };
+  if (ConfirmBtnText) {infoJSON.action.text = ConfirmBtnText.toString();};
+  if (isWarning) {infoJSON.type = "error";};
+  if (duration) {infoJSON.duration = parseInt(duration.toString());};
+  if (onclick) {infoJSON.action.click = onclick;};
+  if (align) {infoJSON.align = ["auto", "top", "bottom"][ align.toString().match(/\d+/) % 3 ];};
+  if (icon) {infoJSON.icon = icon;};
+  customElements.get("s-snackbar").builder(infoJSON);
+  return infoJSON;
+};
+/*引入pmd里的存储api*/const pmdStorage={Cookies:{set:function(e,t,o,n){const s=`${encodeURIComponent(e)}=${encodeURIComponent(t)}`;if(o){const e=new Date;e.setTime(e.getTime()+1e3*o),document.cookie=`${s}; expires=${e.toUTCString()}; path=${n}`}else document.cookie=`${s}; path=${n}`},get:function(e){const t=document.cookie.split("; ");for(const o of t){const[t,n]=o.split("=",2);if(decodeURIComponent(t)===e)return decodeURIComponent(n)}return null},remove:function(e){this.set(e,"",{expires:-1})},getAll:function(){const e=document.cookie.split("; "),t={};for(const o of e){const[e,n]=o.split("=",2);t[decodeURIComponent(e)]=decodeURIComponent(n)}return t},reset_dangerous:function(){const e=this.getAll();for(const t in e)this.remove(t)}},Local:{set:function(e,t){localStorage.setItem(e,JSON.stringify(t))},get:function(e){const t=localStorage.getItem(e);try{return JSON.parse(t)}catch(e){return t}},remove:function(e){localStorage.removeItem(e)},getAll:function(){const e={};for(let t=0;t<localStorage.length;t++){const o=localStorage.key(t);e[o]=this.get(o)}return e},reset_dangerous:function(){localStorage.clear()}},Session:{set:function(e,t){sessionStorage.setItem(e,JSON.stringify(t))},get:function(e){const t=sessionStorage.getItem(e);try{return JSON.parse(t)}catch(e){return t}},remove:function(e){sessionStorage.removeItem(e)},getAll:function(){const e={};for(let t=0;t<sessionStorage.length;t++){const o=sessionStorage.key(t);e[o]=this.get(o)}return e},reset_dangerous:function(){sessionStorage.clear()}}};
 
+/**TODO 首页施工自动跳转文档 */openURL("./doc",true)
+
+//safari user-scalable=no
+document.addEventListener('gesturestart', (event) => event.preventDefault())
+
+//暗黑模式隐式支持
+function ChangeColorTheme(target, animationCenter) {
+  if /* 若传入无效动画中心元素则指定为默认元素 */ (!(animationCenter instanceof HTMLElement)) { animationCenter = pageElements.main.root; };
+  return pageElements.root.toggle(target, animationCenter);
+};
+if (!!pmdStorage.Cookies.get("pmd-prefer_color_theme")) {
+  /*如果检测到Cookies中相关设置则启用用户偏好配色，即继承文档配色设置*/
+  if (pmdStorage.Cookies.get("pmd-prefer_color_theme") == "dark") {ChangeColorTheme("dark")};
+  if (pmdStorage.Cookies.get("pmd-prefer_color_theme") == "light") {ChangeColorTheme("light")};
+};
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    ChangeColorTheme("dark");
+  } else {
+    ChangeColorTheme("light");
+  };
+});
+
+//移除no_script标签
 document.getElementById(`no_script`).remove();
