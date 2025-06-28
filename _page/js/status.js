0c3c21a676b0378ab54a0f77cf744db2cafb7896
kdxiaoyi [mipad7] <kdxiaoyi@outlook.com>
2025-06-28 12:57:54 +0000

    完成pmd功能

--- _page/js/status.js
+++ _page/js/status.js
@@ -115,10 +115,72 @@ pageElements = {
 };
 
 //PMD框架相关处理
+/* 自定义Style */
+if (!!conf.info.style) {
+  let styleEle = document.createElement("style");
+  styleEle.innerHTML = conf.info.style;
+  document.body.appendChild(styleEle);
+}
+/* 侧栏内容覆写 */
 pageElements.content.lsidebar.slot1.innerHTML = `<div slot="image"><img title="${conf.sidebar.solt_1.title}" alt="${conf.sidebar.solt_1.alt}" class="ui-img sidebar_img" pmduiimg="true" src="${conf.sidebar.solt_1.src}"></div><div slot="headline"><span>${conf.sidebar.solt_1.alt}</span></div>`;
 pageElements.content.lsidebar.slot2.innerHTML = conf.sidebar.solt_2.innerHTML;
 pageElements.content.lsidebar.slot4.saying.innerHTML = `<center>${conf.info.saying}</center>`;
 pageElements.content.lsidebar.slot4.license.innerHTML = `<center><small>以<a href="${conf.info.licen.link}">${conf.info.licen.what}</a>协议提供内容</small></center>`;
+/* 配色功能 */
+function ChangeColorTheme(target, animationCenter) {
+  if /* 若传入无效动画中心元素则指定为侧栏按钮 */ (!(animationCenter instanceof HTMLElement)) { animationCenter = pageElements.appbar.menuBtn; };
+  return pageElements.pageRoot.toggle(target, animationCenter);
+};
+if (!!pmdStorage.Cookies.get("pmd-prefer_color_theme")) {
+  /*如果检测到Cookies中相关设置则启用用户偏好配色*/
+  if (pmdStorage.Cookies.get("pmd-prefer_color_theme") == "dark") {
+    pageElements.content.lsidebar.slot3.user_setting.color.root.value = "dark";
+  };
+  if (pmdStorage.Cookies.get("pmd-prefer_color_theme") == "light") {
+    pageElements.content.lsidebar.slot3.user_setting.color.root.value = "light";
+  };
+};
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+  if (pageElements.content.lsidebar.slot3.user_setting.color.root.value != "auto") {return;};
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    ChangeColorTheme("dark");
+  } else {
+    ChangeColorTheme("light");
+  };
+});
+pageElements.content.lsidebar.slot3.user_setting.color.root.addEventListener("change", () => {
+  if (pageElements.content.lsidebar.slot3.user_setting.color.root.value == "auto") {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      ChangeColorTheme("dark", pageElements.content.lsidebar.slot3.user_setting.color.auto);
+    } else {
+      ChangeColorTheme("light", pageElements.content.lsidebar.slot3.user_setting.color.auto);
+    };
+    pmdStorage.Cookies.set("pmd-prefer_color_theme", "auto", 2147483647, "/");
+  };
+  if (pageElements.content.lsidebar.slot3.user_setting.color.root.value == "dark") {
+    ChangeColorTheme("dark", pageElements.content.lsidebar.slot3.user_setting.color.dark);
+    pmdStorage.Cookies.set("pmd-prefer_color_theme", "dark", 2147483647, "/");
+  };
+  if (pageElements.content.lsidebar.slot3.user_setting.color.root.value == "light") {
+    ChangeColorTheme("light", pageElements.content.lsidebar.slot3.user_setting.color.light);
+    pmdStorage.Cookies.set("pmd-prefer_color_theme", "light", 2147483647, "/");
+  };
+});
+/* 时间显示 */
+function RefreshCountup(countupY, countupM, countupD) {
+  /*计算时间差，JS月份从0开始要减1*/
+  let timeDifference = Date.now() - new Date(countupY, countupM - 1, countupD);
+  /*转换日期差*/
+  let countupD_ = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  let countupH = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let countupM_ = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  let countupS = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  /*更新显示*/
+  pageElements.content.lsidebar.slot4.time.innerHTML = `<center><small>本站已建立${countupD_}天${countupH}小时${countupM_}分钟${countupS}秒</small></center>`;
+};
+if (conf.info.time[0] && !conf.sidebar.replacement) {
+  pageElements.content.lsidebar.slot4._.timeCountInterval = setInterval(() => {RefreshCountup(conf.info.time[1],conf.info.time[2],conf.info.time[3])}, 1000);
+} else {pageElements.content.lsidebar.slot4.time.remove();};
 
 //发起请求
 if (
